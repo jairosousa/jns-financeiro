7cd624db1c625e9763f6f4e9d46b35fba64233a2
jaironsousa1 <jaironsousa@gmail.com>
2018-11-28 19:36:05 -0300

    Merge jhipste novas alterações criada classe cartão e modificação em pagamento

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -5,7 +5,6 @@ import { JnsFinanceiroFornecedorModule } from './fornecedor/fornecedor.module';
 import { JnsFinanceiroCategoriaModule } from './categoria/categoria.module';
 import { JnsFinanceiroEnderecoModule } from './endereco/endereco.module';
 import { JnsFinanceiroPagamentoModule } from './pagamento/pagamento.module';
-import { JnsFinanceiroParcelaModule } from 'app/entities/parcela/parcela.module';
 import { JnsFinanceiroParcelaModule } from './parcela/parcela.module';
 import { JnsFinanceiroCartaoModule } from './cartao/cartao.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
