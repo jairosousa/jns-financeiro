8a8c65afdf28c9b72d1af9101e1ff99bf6a70203
jaironsousa1 <jaironsousa@gmail.com>
2018-12-06 16:29:05 -0300

    Ajuste no Formato de Data no formulario NgbDataPicker

--- src/main/webapp/app/shared/shared.module.ts
+++ src/main/webapp/app/shared/shared.module.ts
@@ -1,15 +1,19 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { NgbDateAdapter } from '@ng-bootstrap/ng-bootstrap';
+import { NgbDateAdapter, NgbDateParserFormatter } from '@ng-bootstrap/ng-bootstrap';
 
 import { NgbDateMomentAdapter } from './util/datepicker-adapter';
 import { JnsFinanceiroSharedLibsModule, JnsFinanceiroSharedCommonModule, JhiLoginModalComponent, HasAnyAuthorityDirective } from './';
 import { BreadCrumbComponent } from './components/bread-crumb/bread-crumb.component';
 import { RouterModule } from '@angular/router';
+import { NgbDateCustomParserFormatter } from 'app/shared/util/NgbDateCustomParserFormatter';
 
 @NgModule({
     imports: [JnsFinanceiroSharedLibsModule, JnsFinanceiroSharedCommonModule, RouterModule],
     declarations: [JhiLoginModalComponent, HasAnyAuthorityDirective, BreadCrumbComponent],
-    providers: [{ provide: NgbDateAdapter, useClass: NgbDateMomentAdapter }],
+    providers: [
+        { provide: NgbDateAdapter, useClass: NgbDateMomentAdapter },
+        { provide: NgbDateParserFormatter, useClass: NgbDateCustomParserFormatter }
+    ],
     entryComponents: [JhiLoginModalComponent],
     exports: [JnsFinanceiroSharedCommonModule, JhiLoginModalComponent, HasAnyAuthorityDirective, BreadCrumbComponent, RouterModule],
     schemas: [CUSTOM_ELEMENTS_SCHEMA]
